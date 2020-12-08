8df388b946b7f5c4d9b631b043312bb44593143d
Eduardo Souza <dado.eletrons@gmail.com>
2020-12-08 01:15:39 -0300

    Updates

---
+++ precache-manifest.75b3613b16b98c54586fbdeb2b2eda70.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "264f2d1b243a1e21cab2c44e3f627953",
+    "url": "/Trybe_Exercicio_16-2_Edu_Souza--part-3-final/index.html"
+  },
+  {
+    "revision": "866658bfefa109e7e6f1",
+    "url": "/Trybe_Exercicio_16-2_Edu_Souza--part-3-final/static/css/main.c45e22ea.chunk.css"
+  },
+  {
+    "revision": "613a469b3327a593a637",
+    "url": "/Trybe_Exercicio_16-2_Edu_Souza--part-3-final/static/js/2.0cc67d65.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Trybe_Exercicio_16-2_Edu_Souza--part-3-final/static/js/2.0cc67d65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "866658bfefa109e7e6f1",
+    "url": "/Trybe_Exercicio_16-2_Edu_Souza--part-3-final/static/js/main.7dd7df90.chunk.js"
+  },
+  {
+    "revision": "5a687b8da17d0a0cc540",
+    "url": "/Trybe_Exercicio_16-2_Edu_Souza--part-3-final/static/js/runtime-main.3c07f3e0.js"
+  },
+  {
+    "revision": "38352d9ee02244a39dffad1621609212",
+    "url": "/Trybe_Exercicio_16-2_Edu_Souza--part-3-final/static/media/carBlue.38352d9e.jpeg"
+  },
+  {
+    "revision": "a6362bdc70f08eb38f428bf8c3895cd3",
+    "url": "/Trybe_Exercicio_16-2_Edu_Souza--part-3-final/static/media/carRed.a6362bdc.jpeg"
+  },
+  {
+    "revision": "a1afa5a3cdd10b52a2df7bde7bdb7d1e",
+    "url": "/Trybe_Exercicio_16-2_Edu_Souza--part-3-final/static/media/carYellow.a1afa5a3.jpeg"
+  },
+  {
+    "revision": "35d4985a099392cd8bb0f48afe0f6424",
+    "url": "/Trybe_Exercicio_16-2_Edu_Souza--part-3-final/static/media/greenSignal.35d4985a.jpeg"
+  },
+  {
+    "revision": "1272fbd8065a0bcd6d148c7bbf20c302",
+    "url": "/Trybe_Exercicio_16-2_Edu_Souza--part-3-final/static/media/redSignal.1272fbd8.jpeg"
+  },
+  {
+    "revision": "d99fdf2acda5fa3f088745272ae1711b",
+    "url": "/Trybe_Exercicio_16-2_Edu_Souza--part-3-final/static/media/yellowSignal.d99fdf2a.jpeg"
+  }
+]);
